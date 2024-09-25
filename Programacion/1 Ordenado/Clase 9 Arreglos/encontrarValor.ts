c15921f00cbf04d728a79c37192788439d8c99b5
santysnk <santysnk@gmail.com>
2024-09-25 20:01:58 -0300

    agrego el archivo encontrarValor.ts

---
+++ Programacion/1 Ordenado/Clase 9 Arreglos/encontrarValor.ts
@@ -0,0 +1,27 @@
+
+/*
+Estructuras de Datos
+
+Encontrar el elemento más grande del arreglo
+
+Dado el siguiente arreglo [4,7,9,3,1,45,67,23,29,78,11,16]:
+
+    - Crear un programa que encuentre cuál es el número más grande del arreglo e imprimirlo por consola.
+    - Almacenar el número más grande en una variable global y pasarlo a una función para determinar si el
+    número es par o impar.
+*/
+
+console.clear()
+console.log("Encontrar el elemento más grande del arreglo");
+console.log("--------------------------------------------");
+
+let numArray: number[] = [4, 7, 9, 3, 1, 45, 67, 23, 29, 78, 11, 16];
+let mayor:number = 0;
+
+// Encontrar el número mayor
+for (let i:number = 0; i < numArray.length;i++){
+    if (mayor < numArray[i]){
+        mayor = numArray[i];
+    }
+}
+console.log(`\nEl numero mayor en el array es: ${mayor}`);
